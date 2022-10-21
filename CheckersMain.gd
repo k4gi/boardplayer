89ac0d185cb14726833447d89861da041c1f40c0
kagi <k4gi@pm.me>
2022-10-21 15:25:58 +1100

    main menu is working now

--- CheckersMain.gd
+++ CheckersMain.gd
@@ -1,7 +1,21 @@
 extends Node2D
 
 
-func _ready():
+const CHECKERS_GAME = preload("res://CheckersGame.tscn")
+
+
+var CheckersGame = null
+
+
+func create_game():
+	get_node("%MainMenu").set_visible(false)
+	
+	CheckersGame = CHECKERS_GAME.instantiate()
+	CheckersGame.set_position(Vector2i(128,0))
+	CheckersGame.connect("turn_toggled", _on_checkers_game_turn_toggled)
+	CheckersGame.connect("game_won", _on_checkers_game_game_won)
+	add_child(CheckersGame)
+	
 	get_node("%TurnDisplay").set_visible(true)
 
 
@@ -19,3 +33,7 @@ func _on_checkers_game_game_won(turn, score):
 
 func _on_play_again_pressed():
 	get_tree().reload_current_scene()
+
+
+func _on_local_game_pressed():
+	create_game()
