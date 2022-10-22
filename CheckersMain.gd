5862f28f9b51ad38203f63f3def9e689cbb151fd
kagi <k4gi@pm.me>
2022-10-22 17:56:13 +1100

    i suppose this is how you do multiplayer (!)

--- CheckersMain.gd
+++ CheckersMain.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 
 const CHECKERS_GAME = preload("res://CheckersGame.tscn")
+const PLAYER = preload("res://Player.tscn")
+
+
+var network_address = "localhost"
+var network_port = 54321
 
 
 var CheckersGame = null
@@ -12,8 +17,8 @@ func create_game():
 	
 	CheckersGame = CHECKERS_GAME.instantiate()
 	CheckersGame.set_position(Vector2i(128,0))
-	CheckersGame.connect("turn_toggled", _on_checkers_game_turn_toggled)
-	CheckersGame.connect("game_won", _on_checkers_game_game_won)
+	CheckersGame.turn_toggled.connect(_on_checkers_game_turn_toggled)
+	CheckersGame.game_won.connect(_on_checkers_game_game_won)
 	add_child(CheckersGame)
 	
 	get_node("%TurnDisplay").set_visible(true)
@@ -40,7 +45,45 @@ func _on_local_game_pressed():
 
 
 func _on_host_game_pressed():
-	print("hello")
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_server(9999)
-	get_tree().get_multiplayer().set_multiplayer_peer(peer)
+	# Listen to peer connections, and create new player for them
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	# Listen to peer disconnections, and destroy their players
+	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+	
+	peer.create_server(network_port)
+
+	multiplayer.set_multiplayer_peer(peer)
+	
+	create_player(1)
+
+
+func _on_peer_connected(id):
+	create_player(id)
+
+
+func _on_peer_disconnected(id):
+	remove_player(id)
+
+
+func create_player(id):
+	var new_player = PLAYER.instantiate()
+	new_player.set("peer_id", id)
+	$Players.add_child(new_player)
+	print("player created %d" % id)
+
+
+func remove_player(id):
+	for each_player in $Players.get_children():
+		if each_player.get("peer_id") == id:
+			$Players.remove_child(each_player)
+			each_player.queue_free()
+			print("player removed %d" % id)
+
+
+func _on_join_game_pressed():
+	var peer = ENetMultiplayerPeer.new()
+	
+	peer.create_client(network_address, network_port)
+
+	multiplayer.set_multiplayer_peer(peer)
