9060319850728d954dffffef5fa99b57e18c3316
kagi <k4gi@pm.me>
2023-02-17 12:04:58 +1100

    when godot 4 rc1 crashes and you lose like 2 minutes of work

--- boardplayer-server/OnlineReady.gd
+++ boardplayer-server/OnlineReady.gd
@@ -1,7 +1,23 @@
 extends Node
 
 
-signal ready_button_pressed(button_pressed, remote_sender)
-signal start_game_pressed(remote_sender)
+signal ready_pressed(button_pressed, remote_sender)
+signal start_pressed(remote_sender)
 
 
+@rpc("any_peer", "reliable")
+func ready_button_pressed(button_pressed):
+	var remote_sender = multiplayer.get_remote_sender_id()
+	emit_signal("ready_pressed", button_pressed, remote_sender)
+	rpc_id(remote_sender, "ready_button_received")
+
+
+@rpc("any_peer", "reliable")
+func start_game_pressed():
+	var remote_sender = multiplayer.get_remote_sender_id()
+	emit_signal("start_pressed", remote_sender)
+
+
+@rpc("reliable")
+func ready_button_received():
+	pass #dummy
--- boardplayer/OnlineReady.gd
+++ boardplayer/OnlineReady.gd
@@ -1,9 +1,32 @@
 extends HBoxContainer
 
 
+@onready var ReadyButton = $VBox/HBoxReady/ReadyButton
+@onready var StartGame = $VBox/HBoxReady/StartGame
+
+
 func _on_ready_button_toggled(button_pressed):
-	pass # Replace with function body.
+	ReadyButton.set_disabled(true)
+	rpc_id(1, "ready_button_pressed", button_pressed)
 
 
 func _on_start_game_pressed():
-	pass # Replace with function body.
+	StartGame.set_disabled(true)
+	rpc_id(1, "start_game_pressed")
+
+
+@rpc("any_peer", "reliable")
+func ready_button_pressed(button_pressed):
+	pass #dummy
+
+
+@rpc("any_peer", "reliable")
+func start_game_pressed():
+	pass #dummy
+
+
+@rpc("reliable")
+func ready_button_received():
+	ReadyButton.set_disabled(false)
+
+
