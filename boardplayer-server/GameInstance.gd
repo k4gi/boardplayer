8763c3e2604691f0bf25ca4316102cce0742a14f
kagi <k4gi@pm.me>
2023-02-23 12:01:32 +1100

    better, simpler way to know which squares are red, and that fixes piece placement for the server.

--- boardplayer-server/GameInstance.gd
+++ boardplayer-server/GameInstance.gd
@@ -9,8 +9,6 @@ const TILE_BEIGE = Vector2i(0,0)
 const TILE_RED = Vector2i(1,0)
 
 
-var board_array = []
-
 var piece_array = []
 
 var score = {
@@ -36,27 +34,19 @@ func set_ready(button_pressed, remote_sender):
 func start_game():
 	emit_signal("hide_online_ready_for_players", client_peer_ids)
 	
-	#build piece_array and board_array
-	var tile_toggle_red = false
-	
+	#build piece_array
 	for x in range(8):
 		piece_array.append( [] )
-		board_array.append( [] )
 		for y in range(8):
 			piece_array[x].append( null )
-			if tile_toggle_red:
-				board_array[x].append( TILE_RED )
-			else:
-				board_array[x].append( TILE_BEIGE )
-			tile_toggle_red = !tile_toggle_red
 	
 	for first_three in range(0,3):
 		for x in range(8):
-			if board_array[x][first_three] == TILE_RED:
+			if (x%2 == 0) != (first_three%2 == 0):
 				spawn_piece(x, first_three, "down", "white")
 	for last_three in range(5,8):
 		for x in range(8):
-			if board_array[x][last_three] == TILE_RED:
+			if (x%2 == 0) != (last_three%2 == 0):
 				spawn_piece(x, last_three, "up", "black")
 	
 	get_parent().sync_board_with(client_peer_ids, piece_array)
