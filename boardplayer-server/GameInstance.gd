e79dbaf079dabaff63e9902dd31d38e66a57e01a
kagi <k4gi@pm.me>
2023-02-25 15:00:45 +1100

    it _looks_ like i can basically just copy the highlight method from CheckersGame . . .

--- boardplayer-server/GameInstance.gd
+++ boardplayer-server/GameInstance.gd
@@ -60,9 +60,38 @@ func spawn_piece(x, y, facing, colour):
 	piece_array[x][y] = new_piece
 
 
-func get_highlights(piece_pos):
+func get_highlights(piece_pos, jumps_only = false):
 	var output_highlights = []
 	# a "highlight" is a dictionary, let's say
 	#with a position x y in the grid
 	#and a taking_piece position x y in the grid. for when you're jumping. so we can keep track of who to delete.
-	pass
+	
+	var target_rows = []
+	if piece_array[piece_pos.x][piece_pos.y]["can_move"].has("up") and piece_pos.y-1 >= 0:
+		target_rows.append( -1 )
+	if piece_array[piece_pos.x][piece_pos.y]["can_move"].has("down") and piece_pos.y+1 < 8:
+		target_rows.append( 1 )
+	
+	for y_diff in target_rows:
+		for x_diff in [-1, 1]:
+			var target_spot = Vector2i(piece_pos.x+x_diff, piece_pos.y+y_diff)
+			
+			if target_spot.x >= 0 and target_spot.x < 8:
+				var occupying_piece = piece_array[target_spot.x][target_spot.y]
+				
+				if occupying_piece == null and not jumps_only:
+					var new_highlight = {}
+					new_highlight["pos"] = target_spot
+					new_highlight["taking_piece_pos"] = null
+					output_highlights.append( new_highlight )
+				
+				elif occupying_piece != null and occupying_piece["allegiance"] != piece_array[piece_pos.x][piece_pos.y]["allegiance"]:
+					var second_target_spot = Vector2i(piece_pos.x+(x_diff*2), piece_pos.y+(y_diff*2))
+					if second_target_spot.x >= 0 and second_target_spot.x < 8 and second_target_spot.y >= 0 and second_target_spot.y < 8:
+						var second_occupying_piece = piece_array[second_target_spot.x][second_target_spot.y]
+						if second_occupying_piece == null:
+							var new_highlight = {}
+							new_highlight["pos"] = second_target_spot
+							new_highlight["taking_piece_pos"] = target_spot
+							output_highlights.append( new_highlight )
+
